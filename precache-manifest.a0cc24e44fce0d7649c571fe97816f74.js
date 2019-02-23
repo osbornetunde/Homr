f32251a09c4bbba8501e1d52a708fd171e510dcb
osbornetunde <osbornetunde@gmail.com>
2019-02-23 15:07:56 +0100

    Updates

---
+++ precache-manifest.a0cc24e44fce0d7649c571fe97816f74.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a87bef35568ac5e61c8744fe1337879",
+    "url": "/Homr/static/media/kaduna.2a87bef3.png"
+  },
+  {
+    "revision": "db7ee06294a844f87ff8",
+    "url": "/Homr/static/css/main.6ecb3ff3.chunk.css"
+  },
+  {
+    "revision": "6172248d5f9855eb6659",
+    "url": "/Homr/static/js/runtime~main.6172248d.js"
+  },
+  {
+    "revision": "b2f899ba0b742cdc2c29",
+    "url": "/Homr/static/js/2.b2f899ba.chunk.js"
+  },
+  {
+    "revision": "d9935a020cbf0487df9c2c009c0c365b",
+    "url": "/Homr/static/media/person.d9935a02.png"
+  },
+  {
+    "revision": "a7811e2b67043b2808f5b126747ea384",
+    "url": "/Homr/static/media/apartment2.a7811e2b.png"
+  },
+  {
+    "revision": "3aad8dba8838af5cc0f37322802b436e",
+    "url": "/Homr/static/media/apartment3.3aad8dba.png"
+  },
+  {
+    "revision": "35bfaf14bad64f537cad2c94a8b49103",
+    "url": "/Homr/static/media/apartment4.35bfaf14.png"
+  },
+  {
+    "revision": "33fdcaf0317e3104392ced026098f9a6",
+    "url": "/Homr/static/media/lagos.33fdcaf0.png"
+  },
+  {
+    "revision": "db7ee06294a844f87ff8",
+    "url": "/Homr/static/js/main.db7ee062.chunk.js"
+  },
+  {
+    "revision": "5dd48e13c0faa29145be8fc7beca092f",
+    "url": "/Homr/static/media/abuja.5dd48e13.png"
+  },
+  {
+    "revision": "c3b6ead349a1f881db55fba7efd6ffad",
+    "url": "/Homr/static/media/kebbi.c3b6ead3.png"
+  },
+  {
+    "revision": "0f6c5ccbac90458484ff800f3e6f2a59",
+    "url": "/Homr/static/media/rivers.0f6c5ccb.png"
+  },
+  {
+    "revision": "b1c7d16fcb9caafd75d6472047ecb944",
+    "url": "/Homr/static/media/restroom.b1c7d16f.png"
+  },
+  {
+    "revision": "6b45a6367479ebb573faff8de0c3810e",
+    "url": "/Homr/static/media/apartment1.6b45a636.png"
+  },
+  {
+    "revision": "60b49d1e64ee4b409deccba01c3a4c6d",
+    "url": "/Homr/static/media/apartment5.60b49d1e.png"
+  },
+  {
+    "revision": "1f085deb600ecf2fc13958b47faaff2d",
+    "url": "/Homr/static/media/apartment6.1f085deb.png"
+  },
+  {
+    "revision": "0ee702f028182572b958194388aba66c",
+    "url": "/Homr/static/media/landingpage.0ee702f0.png"
+  },
+  {
+    "revision": "27e326cff7e028d22dc6365ce8788a01",
+    "url": "/Homr/index.html"
+  }
+];
